3d5d9c7ba64d6e7d8b1620fd407176c76b1db501
root <root@golgoth>
2017-11-10 08:39:12 +0100

    update Kaldi

--- scripts/path.sh
+++ scripts/path.sh
@@ -2,29 +2,5 @@
 #lvcsrRootDir=/media/storage0/ubuntu-abdel-data/PIPE_STT/offline-decoding
 lvcsrRootDir=/opt/speech-to-text
 export KALDI_ROOT=$lvcsrRootDir/tools/kaldi
-export PATH=$PWD/tools/festival/nsw/bin:$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
-[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
-. $KALDI_ROOT/tools/config/common_path.sh
-export LANG=C
-export LANGUAGE=C
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/nnetbin:$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/kwsbin:$KALDI_ROOT/src/online2bin/:$KALDI_ROOT/src/ivectorbin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH
 export LC_ALL=C
-
-# we use this both in the Data prepare (Normalization step) and in optional way in the LM training and the G2P-related scripts
-PYTHON='python2.7'
-PYTHON3='python3'
-
-### Below are the paths used by the optional parts of the recipe
-
-# We only need the Festival stuff below for the optional text normalization(for LM-training) step
-FEST_ROOT=tools/festival
-NSW_PATH=${FEST_ROOT}/festival/bin:${FEST_ROOT}/nsw/bin
-export PATH=$PATH:$NSW_PATH
-
-# SRILM is needed for LM model building
-SRILM_ROOT=$KALDI_ROOT/tools/srilm
-SRILM_PATH=$SRILM_ROOT/bin:$SRILM_ROOT/bin/i686-m64
-export PATH=$PATH:$SRILM_PATH
-
-# Sequitur G2P executable
-sequitur=$KALDI_ROOT/tools/sequitur/g2p.py
-sequitur_path="$(dirname $sequitur)/lib/$PYTHON/site-packages"
